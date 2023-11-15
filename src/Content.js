7edb15153a5a1ebeb6e428b25a5865509867a073
Sarbudeen <sarbudeenmca@gmail.com>
2023-11-16 00:05:33 +0400

    UseState Finished

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Content = () => {
-  return (
-    <main>Content</main>
-  )
-}
 
-export default Content
+  function names() {
+    const name = ["Hello", "Hi", "Hey", "Wow", "Good"];
+    const rand = Math.floor(Math.random() * name.length);
+    return name[rand];
+  }
+
+  const [cart, setCart] = useState(() => 99);
+  const [name, setName] = useState(() => names());
+
+  const handleName = () => {
+    setName(names());
+  }
+
+  const handleRemove = () => {
+    setCart(cart - 1);
+  };
+
+  const handleAdd = () => {
+    setCart((prevCart) => { return prevCart + 1 });
+  };
+
+  return (
+    <main>
+      <div className="box">
+        <button onClick={handleRemove}>Remove (-)</button>
+        <h1>{cart}</h1>
+        <button onClick={handleAdd}>Add (+)</button>
+      </div>
+
+      <div className="box">
+        <button onClick={handleName}>Change</button>
+        <h1>{name}</h1>
+      </div>
+    </main >
+  );
+};
+
+export default Content;
